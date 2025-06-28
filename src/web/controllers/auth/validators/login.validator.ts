1ed30d2f15d562a60d2d2a816c77ae75da2b79fa
douqin <dagxuanlam@gmail.com>
2025-06-28 08:59:28 +0700

    fix: remove global validator for identifier field of login

--- src/web/controllers/auth/validators/login.validator.ts
+++ src/web/controllers/auth/validators/login.validator.ts
@@ -1,7 +1,7 @@
 import { body } from "express-validator";
 
 const loginValidator = [
-  body("identifier").exists().withMessage("Phone number is required"),
+  body("identifier").exists().withMessage("Phone number or username or email is required"),
   body("password").exists().withMessage("Password is required"),
 ];
 export default loginValidator;
